d4524e79b1878372034242fc3f07231c1cb6e5f2
Lusine Aslanyan <lusine.aslanyan199425@gmail.com>
2024-08-20 12:06:33 -0700

    adding some feature

--- index.ts
+++ index.ts
@@ -2,16 +2,16 @@ import  fs from 'fs';
 import  path from 'path'
 
 
-function directory(dirPath: string ): void {
-
-    try {
-        const items = fs.readdirSync(dirPath); 
+function directory(dirPath: string, space: number = 0 ): void {
+    const items = fs.readdirSync(dirPath)
+    const indentation = ' '.repeat(space * 2)
+    try { 
         items.forEach(item => {
             const fullPath = path.join(dirPath, item); 
             const stats = fs.statSync(fullPath); 
             if (stats.isFile()) {
-                
-                console.log(` ${item}`);
+                console.log(` ${indentation} ${item}`);
+                directory(dirPath, space + 1)
             } 
         })}catch(err){
             console.log(err)
@@ -20,3 +20,4 @@ function directory(dirPath: string ): void {
     }
 
 
+directory('./src')
